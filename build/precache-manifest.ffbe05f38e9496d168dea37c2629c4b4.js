e637dd7abf85061e5c4f0dfee663945ed35b406c
jonitaajamo <joni.taajamo@outlook.com>
2019-02-11 15:38:59 +0200

    added build again with proxy

---
+++ build/precache-manifest.ffbe05f38e9496d168dea37c2629c4b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "facdbfb079fe2e3896a0",
+    "url": "/static/css/main.45c3f71c.chunk.css"
+  },
+  {
+    "revision": "facdbfb079fe2e3896a0",
+    "url": "/static/js/main.facdbfb0.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e25abdada0e0bf25eb4e107d67f2a3b8",
+    "url": "/index.html"
+  }
+];
